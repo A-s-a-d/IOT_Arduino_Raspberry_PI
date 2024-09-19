d4f49e8cc846beac05bcd6c91268dd09e8cfda7d
A-s-a-d <81926998+A-s-a-d@users.noreply.github.com>
2024-09-19 09:39:26 +0200

    Modification de programme DHT22 pour le mettre dans un objet

--- lib/DHT22/DHT22.cpp
+++ lib/DHT22/DHT22.cpp
@@ -7,13 +7,53 @@ DHT22::DHT22()
     _humidity = 0;
     _dewPoint = 0;
 }
+
 void DHT22::begin(uint8_t dhtPin)
 {
     dhtPin = _pin;
+    pinMode(_pin, INPUT);
+    delay(2000);
 }
+
 DHT_STATUS DHT22::readValues()
 {
+    uint8_t hum_upper = 0x00;
+    uint8_t hum_lower = 0x00;
+    uint8_t temp_upper = 0x00;
+    uint8_t temp_lower = 0x00;
+    uint8_t checksum = 0x00;
+    uint8_t operation = 0x00;
+    uint16_t temp = 0x0000;
+    uint16_t hum = 0x0000;
+    DHT_STATUS status = DHT_OK;
+
+    status = _startConversion();
+
+    if (status == 0)
+    {
+        hum_upper = _readByte();
+        hum_lower = _readByte();
+        temp_upper = _readByte();
+        temp_lower = _readByte();
+        checksum = _readByte();
+    }
+    operation = hum_upper + hum_lower + temp_upper + temp_lower;
+
+    if (operation == checksum)
+    {
+        status = DHT_OK;
+        hum = (hum_upper << 8) | hum_lower;
+        temp = (temp_upper << 8) | temp_lower;
+        _temperature = temp / 10.0;
+        _humidity = hum / 10.0;
+    }
+    else
+    {
+        return DHT_ERROR_CHECK_SUM;
+    }
+    return status;
 }
+
 float DHT22::getTemperature()
 {
 }
@@ -26,10 +66,70 @@ float DHT22::getDewPoint()
 
 DHT_STATUS DHT22::_startConversion()
 {
+    // var loc
+    DHT_STATUS status;
+    // uc output signal
+    pinMode(_pin, OUTPUT);
+    digitalWrite(_pin, 0);
+    delayMicroseconds(1200);
+    digitalWrite(_pin, 1);
+    delayMicroseconds(30);
+
+    // en mode entree
+    pinMode(_pin, INPUT);
+    bool test;
+    // verification 0
+    delayMicroseconds(40);
+    test = digitalRead(_pin);
+    uint32_t now = millis();
+    if (test == 1)
+    {
+        status = DHT_ERROR_RESPONSE;
+        if (millis() - now > 20)
+        {
+            return DHT_ERROR_TIMEOUT;
+        }
+        else
+        {
+            return DHT_ERROR_RESPONSE;
+        }
+    }
+    else
+    {
+        // verification 1
+        delayMicroseconds(80);
+        test = digitalRead(_pin);
+        if (test == 0)
+        {
+            return DHT_ERROR_RESPONSE;
+        }
+        while (digitalRead(_pin) == 1)
+        {
+        }
+    }
+    return DHT_OK;
 }
+
 uint8_t DHT22::_readByte()
 {
+    uint8_t res = 0x00;
+    for (int i = 0; i < 8; i++)
+    {
+        while (digitalRead(_pin) == 0)
+        {
+        }
+        delayMicroseconds(35);
+        if (digitalRead(_pin) == 1)
+        {
+            res = res | 1 << (7 - i);
+            while (digitalRead(_pin) == 1)
+            {
+            }
+        }
+    }
+    return res;
 }
+
 float DHT22::_readDewPoint(float temp, float hum)
 {
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,10 @@
 #define LED_R 3
 #define LED_G 4
 #define PERIOD 3000
+
 const uint8_t DHT_PIN = 7;
 
 #define actionPeriod 3000
-
-// var global
-float temperature = 0;
-float humidity = 0;
-float dewPoint = 0;
-
-enum DHT_STATUS
-{
-  DHT_OK = 0,
-  DHT_ERROR_RESPONSE,
-  DHT_ERROR_CHECK_SUM,
-  DHT_ERROR_TIMEOUT
-};
 
 // prototypes
 void action();
@@ -73,111 +61,16 @@ void action()
 
 void DHT_begin()
 {
-  pinMode(DHT_PIN, INPUT);
-  delay(2000);
 }
 
 DHT_STATUS DHT_startConversion()
 {
-  // var loc
-  DHT_STATUS status;
-  // uc output signal
-  pinMode(DHT_PIN, OUTPUT);
-  digitalWrite(DHT_PIN, 0);
-  delayMicroseconds(1200);
-  digitalWrite(DHT_PIN, 1);
-  delayMicroseconds(30);
-
-  // en mode entree
-  pinMode(DHT_PIN, INPUT);
-  bool test;
-  // verification 0
-  delayMicroseconds(40);
-  test = digitalRead(DHT_PIN);
-  uint32_t now = millis();
-  if (test == 1)
-  {
-    status = DHT_ERROR_RESPONSE;
-    if (millis() - now > 20)
-    {
-      return DHT_ERROR_TIMEOUT;
-    }
-    else
-    {
-      return DHT_ERROR_RESPONSE;
-    }
-  }
-  else
-  {
-    // verification 1
-    delayMicroseconds(80);
-    test = digitalRead(DHT_PIN);
-    if (test == 0)
-    {
-      return DHT_ERROR_RESPONSE;
-    }
-    while (digitalRead(DHT_PIN) == 1)
-    {
-    }
-  }
-  return DHT_OK;
 }
 
 uint8_t DHT_readbyte()
 {
-  uint8_t res = 0x00;
-  for (int i = 0; i < 8; i++)
-  {
-    while (digitalRead(DHT_PIN) == 0)
-    {
-    }
-    delayMicroseconds(35);
-    if (digitalRead(DHT_PIN) == 1)
-    {
-      res = res | 1 << (7 - i);
-      while (digitalRead(DHT_PIN) == 1)
-      {
-      }
-    }
-  }
-  return res;
 }
 
 DHT_STATUS DHT_readValues()
 {
-  uint8_t hum_upper = 0x00;
-  uint8_t hum_lower = 0x00;
-  uint8_t temp_upper = 0x00;
-  uint8_t temp_lower = 0x00;
-  uint8_t checksum = 0x00;
-  uint8_t operation = 0x00;
-  uint16_t temp = 0x0000;
-  uint16_t hum = 0x0000;
-  DHT_STATUS status = DHT_OK;
-
-  status = DHT_startConversion();
-
-  if (status == 0)
-  {
-    hum_upper = DHT_readbyte();
-    hum_lower = DHT_readbyte();
-    temp_upper = DHT_readbyte();
-    temp_lower = DHT_readbyte();
-    checksum = DHT_readbyte();
-  }
-  operation = hum_upper + hum_lower + temp_upper + temp_lower;
-
-  if (operation == checksum)
-  {
-    status = DHT_OK;
-    hum = (hum_upper << 8) | hum_lower;
-    temp = (temp_upper << 8) | temp_lower;
-    temperature = temp / 10.0;
-    humidity = hum / 10.0;
-  }
-  else
-  {
-    return DHT_ERROR_CHECK_SUM;
-  }
-  return status;
 }
